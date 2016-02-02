c94ac18c106c4aeff4b3ce9f0adc2b4cdcaa5a26
Andrzej Dopierala <andrzej.dopierala@currency-one.com>
2016-02-02 09:37:51 +0100

    support for VGA more options for check-ssl mk-job-lock - fork of mk-job

--- check-remote-cert.sh
+++ check-remote-cert.sh
@@ -5,5 +5,12 @@ echo invoke:
 echo $0 serwer:port
 exit 0
 fi
+if [ "$2" ]; then
+	shift
+	params=$*
+else
+	params="-noout -subject -issuer -dates -modulus"
+fi
+
 echo | openssl s_client -showcerts -connect ${SERVER} 2>/dev/null | # s_client connects
-openssl x509 -noout -subject -issuer -dates -modulus
+	openssl x509 $params
