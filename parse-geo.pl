5eeee834314f861bbf5ab5d20d320f0cd8134a92
Andrzej Dopierała <undefine@aramin.net>
2023-03-05 16:35:51 +0100

    zcav-plot

--- parse-geo.pl
+++ parse-geo.pl
@@ -5,8 +5,8 @@ use Geo::IP;
 # http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
 # http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 # http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-my $gcity = Geo::IP->open('/usr/share/GeoIP/GeoLiteCity.dat' , GEOIP_STANDARD );
-my $gcountry = Geo::IP->open('/usr/share/GeoIP/GeoIP.dat' , GEOIP_STANDARD );
+#my $gcity = Geo::IP->open('/usr/share/GeoIP/GeoLiteCity.dat' , GEOIP_STANDARD );
+#my $gcountry = Geo::IP->open('/usr/share/GeoIP/GeoIP.dat' , GEOIP_STANDARD );
 my $gasn = Geo::IP->open('/usr/share/GeoIP/GeoIPASNum.dat' , GEOIP_STANDARD );
 my $recordcity,$city,$country,$ip;
 
@@ -15,7 +15,7 @@ $ip=$_;
 chomp($ip);
 if ($ip =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
 $country = $gcountry->country_code_by_addr($ip);
-$asn = $gasn->org_by_addr($ip);
+#$asn = $gasn->name_by_addr($ip);
 # $recordcity = $gcity->record_by_addr($ip);
 
 #$country=$recordcity->country_code;
